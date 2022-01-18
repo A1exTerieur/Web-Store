2587c54c1de356ddc301f3b890c9da227ff07cac
AlexandreTeixeiraDeFreitas <75750185+AlexandreTeixeiraDeFreitas@users.noreply.github.com>
2022-01-18 19:30:18 +0100

    index

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Header from './components/header/Header.js'
-import NavBarApp from './components/NavBarApp/NavBarApp.jsx'
-import Body from './components/body/Body.js'
 ReactDOM.render(
   <React.StrictMode>
     <App />
